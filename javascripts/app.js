2ac54d6b259a022e7b59b8b157effe86fd5c7e0c
vanilla-ice <valeraerohin97@mail.ru>
2015-12-10 20:06:25 +0300

    fixes for program page green

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,365 @@
-!function(){"use strict";var e="undefined"==typeof window?global:window;if("function"!=typeof e.require){var r={},n={},t={}.hasOwnProperty,o={},i=function(e,r){var n=0;r&&(r.indexOf(!1)&&(n="components/".length),r.indexOf("/",n)>0&&(r=r.substring(n,r.indexOf("/",n))));var t=o[e+"/index.js"]||o[r+"/deps/"+e+"/index.js"];return t?"components/"+t.substring(0,t.length-".js".length):e},l=function(){var e=/^\.\.?(\/|$)/;return function(r,n){var t,o,i=[];t=(e.test(n)?r+"/"+n:n).split("/");for(var l=0,a=t.length;a>l;l++)o=t[l],".."===o?i.pop():"."!==o&&""!==o&&i.push(o);return i.join("/")}}(),a=function(e){return e.split("/").slice(0,-1).join("/")},s=function(r){return function(n){var t=l(a(r),n);return e.require(t,r)}},d=function(e,r){var t={id:e,exports:{}};return n[e]=t,r(t.exports,s(e),t),t.exports},u=function(e,o){var a=l(e,".");if(null==o&&(o="/"),a=i(e,o),t.call(n,a))return n[a].exports;if(t.call(r,a))return d(a,r[a]);var s=l(a,"./index");if(t.call(n,s))return n[s].exports;if(t.call(r,s))return d(s,r[s]);throw new Error('Cannot find module "'+e+'" from "'+o+'"')};u.alias=function(e,r){o[r]=e},u.register=u.define=function(e,n){if("object"==typeof e)for(var o in e)t.call(e,o)&&(r[o]=e[o]);else r[e]=n},u.list=function(){var e=[];for(var n in r)t.call(r,n)&&e.push(n);return e},u.brunch=!0,e.require=u}}(),require.register("initialize",function(e,r,n){$(window).load(function(){return $(".preloader").removeClass("hide"),$(".slider").slick({arrows:!1,vertical:!0,initialSlide:1,autoplay:!0,speed:500})}),$(document).ready(function(){return $("#head").find(".logo").removeClass("logo-green"),$("#head").find(".logo").addClass("logo-blue"),$("#head").find(".text-blue").addClass("move-blue"),$("#head").find(".txt").removeClass("move"),$("#head").find(".text-green").removeClass("move-green"),Parse.initialize("WD4SCqCV1MsggPivlA2FvNuwHym2lIWxNhpAmQxu","bgN3ACqo9x6mmfPdtaKUoSvBVw5PFSeMucjiNx8H"),$(".slider-two").slick({itinitalSlide:0}),$(".slider").on("beforeChange",function(e,r,n){return 0===n?($("#head").find(".logo").removeClass("logo-green"),$("#head").find(".logo").addClass("logo-blue"),$("#head").find(".text-blue").addClass("move-blue"),$("#head").find(".txt").removeClass("move"),$("#head").find(".text-green").removeClass("move-green")):1===n?($("#head").find(".logo").removeClass("logo-blue"),$("#head").find(".logo").addClass("logo-green"),$("#head").find(".text-green").addClass("move-green"),$("#head").find(".txt").removeClass("move"),$("#head").find(".text-blue").removeClass("move-blue")):2===n?($("#head").find(".logo").removeClass("logo-green"),$("#head").find(".logo").removeClass("logo-blue"),$("#head").find(".txt").addClass("move"),$("#head").find(".text-blue").removeClass("move-blue"),$("#head").find(".text-green").removeClass("move-green")):void 0}),$(".flex-inner").find(".program").click(function(){return $(".program").removeClass("active"),$(this).addClass("active")}),$("#programs").find("#rule").click(function(){return $(".grid").find(".flex-item").addClass("deactive"),$(".grid").find(".green").removeClass("deactive")}),$("#programs").find("#finan").click(function(){return $(".grid").find(".flex-item").addClass("deactive"),$(".grid").find(".blue").removeClass("deactive")}),$("#programs").find("#edu").click(function(){return $(".grid").find(".flex-item").addClass("deactive"),$(".grid").find(".yellow").removeClass("deactive")}),inlineSVG.init(),$("a[href*=#]").bind("click",function(e){var r;return e.preventDefault(),r=$(this).attr("href"),$("html,body").stop().animate({scrollTop:$(r).offset().top},900,function(){return location.hash=r}),!1}),window.initMap=function(){var e,r,n,t;return e={lat:55.745569,lng:37.66233},r=new google.maps.Map(document.getElementById("map"),{scaleControl:!0,center:e,zoom:16}),n=new google.maps.Marker({position:e,map:r,icon:"images/map_pin.svg"}),t=[{featureType:"water",elementType:"geometry",stylers:[{color:"#007C84"}]},{featureType:"landscape",elementType:"geometry",stylers:[{color:"#0099A1"}]},{featureType:"road",elementType:"geometry",stylers:[{color:"#00AAB4"},{lightness:-37}]},{featureType:"poi",elementType:"geometry",stylers:[{color:"#007C84"}]},{featureType:"transit",elementType:"geometry",stylers:[{color:"#406d80"}]},{elementType:"labels.text.stroke",stylers:[{visibility:"on"},{color:"#3e606f"},{weight:2},{gamma:.84}]},{elementType:"labels.text.fill",stylers:[{color:"#ffffff"}]},{featureType:"administrative",elementType:"geometry",stylers:[{weight:.6},{color:"#1a3541"}]},{elementType:"labels.icon",stylers:[{visibility:"off"}]},{featureType:"poi.park",elementType:"geometry",stylers:[{color:"#007284"}]}],r.setOptions({styles:t,scrollwheel:!1})},window.sr=new scrollReveal,$(".button").click(function(){return swal({title:"Подписка на новости",text:"Введите ваш e-mail:",type:"input",showCancelButton:!0,closeOnConfirm:!1,animation:"slide-from-top",inputPlaceholder:"ваша@почта.ру"},function(e){return e===!1?!1:""===e||-1===e.search(/@/)?(swal.showInputError("Введите e-mail"),!1):Parse.Cloud.run("sendmail",{target:"yuriyurchenko@me.com",originator:"ancor-landing@gmail.com",subject:"e-mail",text:"Email: "+e},{success:function(e){return swal("Отправлено!","Спасибо за подписку","success")},error:function(e){return swal("Ошибка!","Email не отправлен!","error")}})}),$(".cancel").empty().append("Отмена")}),$(".button-prog").click(function(){return swal({title:"Подписка на новости",text:"Введите ваш e-mail:",type:"input",showCancelButton:!0,closeOnConfirm:!1,animation:"slide-from-top",inputPlaceholder:"ваша@почта.ру"},function(e){return e===!1?!1:""===e||-1===e.search(/@/)?(swal.showInputError("Введите e-mail"),!1):Parse.Cloud.run("sendmail",{target:"yuriyurchenko@me.com",originator:"ancor-landing@gmail.com",subject:"e-mail",text:"Email: "+e},{success:function(e){return swal("Отправлено!","Спасибо за подписку","success")},error:function(e){return swal("Ошибка!","Email не отправлен!","error")}})}),$(".cancel").empty().append("Отмена")})})});
+(function() {
+  'use strict';
+
+  var globals = typeof window === 'undefined' ? global : window;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+  var has = ({}).hasOwnProperty;
+
+  var aliases = {};
+
+  var endsWith = function(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+  };
+
+  var unalias = function(alias, loaderPath) {
+    var start = 0;
+    if (loaderPath) {
+      if (loaderPath.indexOf('components/' === 0)) {
+        start = 'components/'.length;
+      }
+      if (loaderPath.indexOf('/', start) > 0) {
+        loaderPath = loaderPath.substring(start, loaderPath.indexOf('/', start));
+      }
+    }
+    var result = aliases[alias + '/index.js'] || aliases[loaderPath + '/deps/' + alias + '/index.js'];
+    if (result) {
+      return 'components/' + result.substring(0, result.length - '.js'.length);
+    }
+    return alias;
+  };
+
+  var expand = (function() {
+    var reg = /^\.\.?(\/|$)/;
+    return function(root, name) {
+      var results = [], parts, part;
+      parts = (reg.test(name) ? root + '/' + name : name).split('/');
+      for (var i = 0, length = parts.length; i < length; i++) {
+        part = parts[i];
+        if (part === '..') {
+          results.pop();
+        } else if (part !== '.' && part !== '') {
+          results.push(part);
+        }
+      }
+      return results.join('/');
+    };
+  })();
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function(name) {
+      var absolute = expand(dirname(path), name);
+      return globals.require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var module = {id: name, exports: {}};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var require = function(name, loaderPath) {
+    var path = expand(name, '.');
+    if (loaderPath == null) loaderPath = '/';
+    path = unalias(name, loaderPath);
+
+    if (has.call(cache, path)) return cache[path].exports;
+    if (has.call(modules, path)) return initModule(path, modules[path]);
+
+    var dirIndex = expand(path, './index');
+    if (has.call(cache, dirIndex)) return cache[dirIndex].exports;
+    if (has.call(modules, dirIndex)) return initModule(dirIndex, modules[dirIndex]);
+
+    throw new Error('Cannot find module "' + name + '" from '+ '"' + loaderPath + '"');
+  };
+
+  require.alias = function(from, to) {
+    aliases[to] = from;
+  };
+
+  require.register = require.define = function(bundle, fn) {
+    if (typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has.call(bundle, key)) {
+          modules[key] = bundle[key];
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+    }
+  };
+
+  require.list = function() {
+    var result = [];
+    for (var item in modules) {
+      if (has.call(modules, item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  };
+
+  require.brunch = true;
+  globals.require = require;
+})();
+require.register("initialize", function(exports, require, module) {
+$(window).load(function() {
+  $('.preloader').removeClass('hide');
+  return $('.slider').slick({
+    arrows: false,
+    vertical: true,
+    initialSlide: 1,
+    autoplay: true,
+    speed: 500
+  });
+});
+
+$(document).ready(function() {
+  $('#head').find('.logo').removeClass('logo-green');
+  $('#head').find('.logo').addClass('logo-blue');
+  $('#head').find('.text-blue').addClass('move-blue');
+  $('#head').find('.txt').removeClass('move');
+  $('#head').find('.text-green').removeClass('move-green');
+  Parse.initialize('WD4SCqCV1MsggPivlA2FvNuwHym2lIWxNhpAmQxu', 'bgN3ACqo9x6mmfPdtaKUoSvBVw5PFSeMucjiNx8H');
+  $('.slider-two').slick({
+    itinitalSlide: 0
+  });
+  $('.slider').on('beforeChange', function(event, slick, direction) {
+    if (direction === 0) {
+      $('#head').find('.logo').removeClass('logo-green');
+      $('#head').find('.logo').addClass('logo-blue');
+      $('#head').find('.text-blue').addClass('move-blue');
+      $('#head').find('.txt').removeClass('move');
+      return $('#head').find('.text-green').removeClass('move-green');
+    } else if (direction === 1) {
+      $('#head').find('.logo').removeClass('logo-blue');
+      $('#head').find('.logo').addClass('logo-green');
+      $('#head').find('.text-green').addClass('move-green');
+      $('#head').find('.txt').removeClass('move');
+      return $('#head').find('.text-blue').removeClass('move-blue');
+    } else if (direction === 2) {
+      $('#head').find('.logo').removeClass('logo-green');
+      $('#head').find('.logo').removeClass('logo-blue');
+      $('#head').find('.txt').addClass('move');
+      $('#head').find('.text-blue').removeClass('move-blue');
+      return $('#head').find('.text-green').removeClass('move-green');
+    }
+  });
+  $('.flex-inner').find('.program').click(function() {
+    $('.program').removeClass('active');
+    return $(this).addClass('active');
+  });
+  $('#programs').find('#rule').click(function() {
+    $('.grid').find('.flex-item').addClass('deactive');
+    return $('.grid').find('.green').removeClass('deactive');
+  });
+  $('#programs').find('#finan').click(function() {
+    $('.grid').find('.flex-item').addClass('deactive');
+    return $('.grid').find('.blue').removeClass('deactive');
+  });
+  $('#programs').find('#edu').click(function() {
+    $('.grid').find('.flex-item').addClass('deactive');
+    return $('.grid').find('.yellow').removeClass('deactive');
+  });
+  inlineSVG.init();
+  $('a[href*=#]').bind('click', function(e) {
+    var target;
+    e.preventDefault();
+    target = $(this).attr("href");
+    $('html,body').stop().animate({
+      scrollTop: $(target).offset().top
+    }, 900, function() {
+      return location.hash = target;
+    });
+    return false;
+  });
+  window.initMap = function() {
+    var cairo, map, marker, styles;
+    cairo = {
+      lat: 55.745569,
+      lng: 37.662330
+    };
+    map = new google.maps.Map(document.getElementById('map'), {
+      scaleControl: true,
+      center: cairo,
+      zoom: 16
+    });
+    marker = new google.maps.Marker({
+      position: cairo,
+      map: map,
+      icon: 'images/map_pin.svg'
+    });
+    styles = [
+      {
+        'featureType': 'water',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#007C84'
+          }
+        ]
+      }, {
+        'featureType': 'landscape',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#0099A1'
+          }
+        ]
+      }, {
+        'featureType': 'road',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#00AAB4'
+          }, {
+            'lightness': -37
+          }
+        ]
+      }, {
+        'featureType': 'poi',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#007C84'
+          }
+        ]
+      }, {
+        'featureType': 'transit',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#406d80'
+          }
+        ]
+      }, {
+        'elementType': 'labels.text.stroke',
+        'stylers': [
+          {
+            'visibility': 'on'
+          }, {
+            'color': '#3e606f'
+          }, {
+            'weight': 2
+          }, {
+            'gamma': 0.84
+          }
+        ]
+      }, {
+        'elementType': 'labels.text.fill',
+        'stylers': [
+          {
+            'color': '#ffffff'
+          }
+        ]
+      }, {
+        'featureType': 'administrative',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'weight': 0.6
+          }, {
+            'color': '#1a3541'
+          }
+        ]
+      }, {
+        'elementType': 'labels.icon',
+        'stylers': [
+          {
+            'visibility': 'off'
+          }
+        ]
+      }, {
+        'featureType': 'poi.park',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#007284'
+          }
+        ]
+      }
+    ];
+    return map.setOptions({
+      styles: styles,
+      scrollwheel: false
+    });
+  };
+  window.sr = new scrollReveal();
+  $('.button').click(function() {
+    swal({
+      title: 'Подписка на новости',
+      text: 'Введите ваш e-mail:',
+      type: 'input',
+      showCancelButton: true,
+      closeOnConfirm: false,
+      animation: 'slide-from-top',
+      inputPlaceholder: 'ваша@почта.ру'
+    }, function(inputValue) {
+      if (inputValue === false) {
+        return false;
+      }
+      if (inputValue === '' || inputValue.search(/@/) === -1) {
+        swal.showInputError('Введите e-mail');
+        return false;
+      }
+      return Parse.Cloud.run('sendmail', {
+        target: 'yuriyurchenko@me.com',
+        originator: 'ancor-landing@gmail.com',
+        subject: 'e-mail',
+        text: "Email: " + inputValue
+      }, {
+        success: function(success) {
+          return swal('Отправлено!', 'Спасибо за подписку', 'success');
+        },
+        error: function(error) {
+          return swal("Ошибка!", "Email не отправлен!", "error");
+        }
+      });
+    });
+    return $('.cancel').empty().append('Отмена');
+  });
+  return $('.button-prog').click(function() {
+    swal({
+      title: 'Подписка на новости',
+      text: 'Введите ваш e-mail:',
+      type: 'input',
+      showCancelButton: true,
+      closeOnConfirm: false,
+      animation: 'slide-from-top',
+      inputPlaceholder: 'ваша@почта.ру'
+    }, function(inputValue) {
+      if (inputValue === false) {
+        return false;
+      }
+      if (inputValue === '' || inputValue.search(/@/) === -1) {
+        swal.showInputError('Введите e-mail');
+        return false;
+      }
+      return Parse.Cloud.run('sendmail', {
+        target: 'yuriyurchenko@me.com',
+        originator: 'ancor-landing@gmail.com',
+        subject: 'e-mail',
+        text: "Email: " + inputValue
+      }, {
+        success: function(success) {
+          return swal('Отправлено!', 'Спасибо за подписку', 'success');
+        },
+        error: function(error) {
+          return swal("Ошибка!", "Email не отправлен!", "error");
+        }
+      });
+    });
+    return $('.cancel').empty().append('Отмена');
+  });
+});
+});
+
+;
+//# sourceMappingURL=app.js.map
